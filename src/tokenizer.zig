2371b16f93297df7f203b75fd4329395c31922ad
Zac <zacrex@outlook.com>
2025-04-17 03:03:15 -0400

    Update tokenizer.zig

--- src/tokenizer.zig
+++ src/tokenizer.zig
@@ -1,10 +1,17 @@
 const std = @import("std");
 const log = @import("log");
-const CommentedToken = @import("tokens").CommentedToken;
-const Comment = @import("tokens").Comment;
-const Token = @import("tokens").Token;
-const Special = @import("tokens").Special;
-const Lambda = @import("tokens").Lambda;
+const CommentedToken = @import("tokens.zig").CommentedToken;
+const Token = @import("tokens.zig").Token;
+const Special = @import("tokens.zig").Special;
+const Lambda = @import("tokens.zig").Lambda;
+const LiteralType = enum {
+    Bool,
+    Null,
+    Integer,
+    Float,
+    String,
+    Symbol,
+};
 
 const EOF = Token.EOF;
 const Newline = Token.Newline;
@@ -60,29 +67,62 @@ const Repeat = Token.Repeat;
 
 const SYMBOL_ENDING = [_]u8{ ' ', '(', ')', '{', '}', '#', ';', '\n', '\t', '\r', '+', '-', '/', '\\', '%', '*', '^', '!', '&', '|', '<', '>', '=', ',', '[', ']', '$', '`', '"' };
 
-const Tokenizer = struct {
-    offset: usize,
+// Generic Iterator
+pub const Iterator = struct {
+    data: []const u8,
     it: usize,
-    current_char: u8,
-    source: std.mem.iterator(u8),
-    raw_source: []const u8,
-    allocator: *std.mem.Allocator,
 
-    pub fn init(input: []const u8, allocator: *std.mem.Allocator) Tokenizer {
+    pub fn init(data: []const u8) Iterator {
+        return Iterator{ .data = data, .it = 0 };
+    }
+
+    pub fn next(self: *Iterator) u8 {
+        if (self.it < self.data.len) {
+            const char = self.data[self.it];
+            self.it += 1;
+            return char;
+        }
+        return 0; // EOF
+    }
+
+    pub fn peek(self: *Iterator) u8 {
+        if (self.it < self.data.len) {
+            return self.data[self.it];
+        }
+        return 0; // EOF
+    }
+
+    pub fn has_next(self: *Iterator) bool {
+        return self.it < self.data.len;
+    }
+};
+
+// Tokenizer using Iterator
+pub const Tokenizer = struct {
+    offset: usize,
+    it: Iterator, // Mutable iterator
+    current_char: u8,
+    raw_source: []const u8,
+    allocator: std.mem.Allocator,
+    tokens: std.ArrayList(CommentedToken),
+
+    pub fn init(input: []const u8, allocator: std.mem.Allocator) Tokenizer {
+        const it = Iterator.init(input);
+        const tokens = std.ArrayList(CommentedToken).init(allocator);
         return Tokenizer{
             .offset = 0,
-            .it = 0,
+            .it = it,
             .current_char = '\\',
-            .source = std.mem.iterator(input),
             .raw_source = input,
             .allocator = allocator,
+            .tokens = tokens,
         };
     }
 
     pub fn tokenize(self: *Tokenizer) []CommentedToken {
         var tokens = std.ArrayList(CommentedToken).init(self.allocator);
         self.next();
-        while (self.it < self.raw_source.len) {
+        while (self.it.has_next()) { // Now 'it' can be mutated
             switch (self.current_char) {
                 ' ' | '\t' => self.next(),
                 '\r' => {
@@ -246,7 +286,6 @@ const Tokenizer = struct {
                     } else if (next_char >= '0' and next_char <= '9') {
                         self.number_literal(&tokens);
                     } else {
-                        log.debug("Found not alphabetic and non-numeric character after a dot. Treating it as an identifier.");
                         self.identifier(&tokens);
                     }
                 },
@@ -258,12 +297,12 @@ const Tokenizer = struct {
                         self.next();
                         self.next();
                     } else {
-                        const custom_binary_start = self.it;
+                        const custom_binary_start = self.it.it;
                         self.next();
                         while (self.current_char != '%') {
                             self.next();
                         }
-                        const custom_binary_end = self.it;
+                        const custom_binary_end = self.it.it;
                         self.push_token(Special(self.raw_source[custom_binary_start..custom_binary_end]), &tokens);
                         self.next();
                     }
@@ -305,7 +344,6 @@ const Tokenizer = struct {
             }
         }
         tokens.append(CommentedToken.new(EOF, self.offset));
-        log.trace("Tokenized: {tokens:?}");
         return tokens.toOwnedSlice();
     }
 
@@ -313,117 +351,12 @@ const Tokenizer = struct {
         tokens.append(CommentedToken.new(token, self.offset));
     }
 
-    fn string_literal(self: *Tokenizer, tokens: *[]CommentedToken) void {
-        const delimiter = self.current_char;
-        const start_offset = self.offset;
-        const start_it = self.it;
-        var in_escape = false;
-        self.next();
-        while (self.current_char != delimiter or in_escape) {
-            self.next();
-            if (in_escape) {
-                in_escape = !in_escape;
-            } else if (self.current_char == '\\') {
-                in_escape = true;
-            }
+    pub fn next(self: *Tokenizer) ?CommentedToken {
+        if (self.tokens.items.len == 0) {
+            return null; // No more tokens
         }
-        tokens.append(CommentedToken.new(Literal(self.raw_source[start_it..self.it]), start_offset));
-    }
-
-    fn number_literal(self: *Tokenizer, tokens: *[]CommentedToken) void {
-        const start_it = self.it;
-        const next_char = self.lookahead();
-        if (self.current_char == '0' and (next_char == 'x' or next_char == 'X')) {
-            self.next();
-            self.next();
-            self.parse_hexadecimal(); // Implement this function if it doesn't exist
-        } else {
-            self.parse_decimal(); // Implement this function if it doesn't exist
-        }
-        self.push_token(Literal(self.raw_source[start_it..self.it]), tokens);
-    }
-
-    fn identifier(self: *Tokenizer, tokens: *[]CommentedToken) void {
-        const start_it = self.it;
-        var in_backticks = false;
-        while ((self.it < self.raw_source.len) and (in_backticks or
-            (self.current_char >= 'a' and self.current_char <= 'z') or
-            (self.current_char >= 'A' and self.current_char <= 'Z') or
-            (self.current_char >= '0' and self.current_char <= '9') or
-            self.current_char == '.' or
-            self.current_char == '_' or
-            self.current_char == '`'))
-        {
-            if (self.current_char == '`') {
-                in_backticks = !in_backticks;
-            }
-            self.next();
-        }
-
-        const symbol = self.raw_source[start_it..self.it];
-        if (std.mem.eql(u8, symbol, "TRUE") or std.mem.eql(u8, symbol, "T")) {
-            self.push_token(Literal("TRUE"), tokens);
-        } else if (std.mem.eql(u8, symbol, "FALSE") or std.mem.eql(u8, symbol, "F")) {
-            self.push_token(Literal("FALSE"), tokens);
-        } else {
-            self.push_token(Symbol(symbol), tokens);
-        }
-    }
-
-    fn identifier_or_reserved(self: *Tokenizer, tokens: *[]CommentedToken) void {
-        const start_it = self.it;
-        while ((self.it < self.raw_source.len) and !SYMBOL_ENDING.contains(self.current_char)) {
-            self.next();
-        }
-        const symbol = self.raw_source[start_it..self.it];
-
-        if (std.mem.eql(u8, symbol, "continue")) {
-            self.push_token(Continue, tokens);
-        } else if (std.mem.eql(u8, symbol, "break")) {
-            self.push_token(Break, tokens);
-        } else if (std.mem.eql(u8, symbol, "for")) {
-            self.push_token(For, tokens);
-        } else if (std.mem.eql(u8, symbol, "if")) {
-            self.push_token(If, tokens);
-        } else if (std.mem.eql(u8, symbol, "else")) {
-            self.push_token(Else, tokens);
-        } else if (std.mem.eql(u8, symbol, "in")) {
-            self.push_token(In, tokens);
-        } else if (std.mem.eql(u8, symbol, "while")) {
-            self.push_token(While, tokens);
-        } else if (std.mem.eql(u8, symbol, "repeat")) {
-            self.push_token(Repeat, tokens);
-        } else if (std.mem.eql(u8, symbol, "function")) {
-            self.push_token(Function, tokens);
-        } else if (std.mem.eql(u8, symbol, "TRUE") or std.mem.eql(u8, symbol, "T")) {
-            self.push_token(Literal("TRUE"), tokens);
-        } else if (std.mem.eql(u8, symbol, "FALSE") or std.mem.eql(u8, symbol, "F")) {
-            self.push_token(Literal("FALSE"), tokens);
-        } else {
-            self.push_token(Symbol(symbol), tokens);
-        }
-    }
-    fn lookahead(self: *Tokenizer) ?u8 {
-        if (self.it + 1 >= self.raw_source.len) return null;
-        return self.raw_source[self.it + 1];
-    }
-
-    fn next(self: *Tokenizer) void {
-        if (self.it < self.raw_source.len) {
-            self.current_char = self.raw_source[self.it];
-            self.offset += 1;
-            self.it += 1;
-        } else {
-            self.current_char = 0;
-        }
-    }
-
-    fn comment(self: *Tokenizer, tokens: *[]CommentedToken) void {
-        const start_it = self.it;
-        self.next();
-        while (self.current_char != '\n' and self.current_char != 0) {
-            self.next();
-        }
-        tokens.append(CommentedToken.comment(Comment(self.raw_source[start_it..self.it])));
+        const token = self.tokens.items[0]; // Get the first token
+        self.tokens.items = self.tokens.items[1..];
+        return token;
     }
 };
